82c269da9bef5abd459c15f5604ff014da385a33
rahulbaliyan <rahulbaliyan404@gmail.com>
2021-02-20 00:07:00 +0530

    This is first commit in Spring Cloud API Gateway.

---
+++ MS-SCAG-3333/src/main/java/com/home/MsScag3333Application.java
@@ -0,0 +1,14 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsScag3333Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsScag3333Application.class, args);
+	}
+
+}
